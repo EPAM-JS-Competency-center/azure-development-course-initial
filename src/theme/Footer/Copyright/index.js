40072459a3c3df729facfc354838f7d0482cbcb8
Nazarii Romankiv <nazarii_romankiv@epam.com>
2024-02-28 16:21:15 +0200

    fix: ebook integration

--- src/theme/Footer/Copyright/index.js
+++ src/theme/Footer/Copyright/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import {LearnFooter} from '@ebook/core';
+
+let LearnFooter = ({ children }) => <>{children}</>; // Default fallback component
+
+if (process.env.NODE_ENV === 'production') {
+    // NODE_ENV is always equal to 'production' for npm run build
+    try {
+        LearnFooter = require('@ebook/core').LearnFooter;
+    } catch(e) {
+        console.error('Failed to load @ebook/access-epm-wrapper:', e);
+    }
+}
+
+
 
 export default function CopyrightWrapper(props) {
     return (
